8c5e13caeeb6ff90e6e0a94b344a678813fb8bde
GaneshKhadsan <ganeshskhadsan@gmail.com>
2019-08-01 12:46:21 +0530

    First Cpp Commit

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+ char fname[30], lname [30];
+
+ cout << " Input First Name: ";
+ cin >> fname;
+ cout << " Input Last Name: ";
+ cin >> lname;
+
+ cout << " Full Name is: "<< fname << " "<< lname <<endl;
+	cout << endl;
+return 0;
+}
